0aa6b67ab2f6804dc93e7afcfa7b597bcc0b0182
willclarktech <will@willclark.tech>
2017-04-21 16:44:56 +0200

    wait for player move

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
+const getPlayerMove = game => {
+
+}
+
 const getComputerMove = game => {
     const moves = game.moves()
     const move = moves[Math.floor(Math.random() * moves.length)]
     game.move(move)
-    console.log(game.ascii())
 }
 
+const waitAndPlayAgain = game => board =>
+  window.setTimeout(
+    play(game).bind(null, (board)),
+    500
+  )
+
 const play = game => board => {
-  game.turn() === 'w'
-    ? getComputerMove(game)
-    : getComputerMove(game)
-
-  board.position(game.fen())
-
-  if (!game.game_over()) {
-    return window.setTimeout(play(game).bind(null, (board)), 500)
+  if (game.turn() === 'b') {
+    getComputerMove(game)
+    board.position(game.fen())
+    console.log(game.ascii())
   }
+
+  if (!game.game_over())
+    return waitAndPlayAgain(game)(board)
 }
 
 const run = () => {
   if (!window.Chess) return console.error('Could not find Chess')
   if (!window.ChessBoard) return console.error('Could not find ChessBoard')
 
-  const board = new ChessBoard('board', 'start')
+  const options = {
+    position: 'start',
+  }
+
+  const board = new ChessBoard('board', options)
   const game = new Chess()
   console.log(game.ascii())
 
